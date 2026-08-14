91d91cfc38a36b73a377f8f47936ef2024ca26c9
urielxdpro25 <urieluncos@gmail.com>
2026-08-14 01:18:44 -0300

    cambios menores

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v9";
+const CACHE = "gestionador-v9.5";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
