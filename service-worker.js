e2170d457f56ab965d75d3c6f406228657d086b8
urielxdpro25 <urieluncos@gmail.com>
2026-09-02 01:56:08 -0300

    añadi el css para todo lo son estilos generales, y termine con los mensajes pith y cambie el logo version 11

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v10";
+const CACHE = "gestionador-v11";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
