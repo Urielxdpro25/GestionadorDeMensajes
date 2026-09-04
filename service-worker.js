f68d09d218a443669c79bd477c22f46d681bf7ba
urielxdpro25 <urieluncos@gmail.com>
2026-09-04 01:48:33 -0300

    version 12, pasando cambios a main

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v11";
+const CACHE = "gestionador-v12";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
