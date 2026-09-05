f1720021f2c00e4f4590874ac74df1af591c78de
urielxdpro25 <urieluncos@gmail.com>
2026-09-05 18:30:43 -0300

    version 13

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v12";
+const CACHE = "gestionador-v13";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
